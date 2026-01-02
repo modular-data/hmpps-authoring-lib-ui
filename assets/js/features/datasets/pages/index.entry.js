8cdb01c121a756ccf9a592fc7c348e001fc48395
Oleksandr Kolesnikov <kolesnikovo@mydigicode.com>
2026-01-02 14:12:15 +0200

    MOJ-407 | Data Product & Dataset home pages init scripts improved

--- assets/js/features/datasets/pages/index.entry.js
+++ assets/js/features/datasets/pages/index.entry.js
@@ -1,11 +1,23 @@
 import { FilterToggleButton } from '@ministryofjustice/frontend'
 
-const filterNode = document.querySelector('[data-module="moj-filter"]')
+const initFilter = () => {
+  const filterNode = document.querySelector('[data-module="moj-filter"]')
 
-const filterToggleButton = new FilterToggleButton(filterNode, {
-  startHidden: true,
-  toggleButton: {
-    showText: 'Show filter',
-    hideText: 'Hide filter',
-  },
-})
+  if (!filterNode) {
+    return undefined
+  }
+
+  return new FilterToggleButton(filterNode, {
+    startHidden: true,
+    toggleButton: {
+      showText: 'Show filter',
+      hideText: 'Hide filter',
+    },
+  })
+}
+
+const init = () => {
+  initFilter()
+}
+
+document.addEventListener('DOMContentLoaded', init)
