1d4e5c655d057fc20600b71f7536be9cb55fe4fe
Oleksandr Kolesnikov <kolesnikovo@mydigicode.com>
2026-01-25 19:13:40 +0200

    MOJ-429 | GDS Components basic Storybook config implemented

---
+++ libs/gds-components/.storybook/main.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { type StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: ['@storybook/addon-docs'],
+  framework: {
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {},
+  },
+};
+
+function getAbsolutePath(value: string): string {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
+}
+
+export default config;
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
