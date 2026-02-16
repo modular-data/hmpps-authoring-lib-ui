5d87631a4922fb6f424e2e7b7fedc2f466263044
Oleksandr Kolesnikov <kolesnikovo@mydigicode.com>
2026-02-16 18:22:35 +0200

    MOJ-462 | Storybook TypeScript support improved (migration to react-docgen-typescript)

---
+++ libs/gds-components/.storybook/config/react-docgen-prop-filter.ts
@@ -0,0 +1,28 @@
+import { type PropFilter } from 'react-docgen-typescript/lib/parser';
+
+const ALWAYS_INCLUDE_PROPS = new Set([
+  'id',
+  'className',
+  'type',
+  'name',
+  'value',
+  'href',
+  'role',
+  'aria-describedby',
+]);
+
+export const reactDocgenPropFilter: PropFilter = (prop) => {
+  if (ALWAYS_INCLUDE_PROPS.has(prop.name)) {
+    return true;
+  }
+
+  const declarations = prop.declarations ?? [];
+
+  if (declarations.length) {
+    return declarations.some(
+      (declaration) => !declaration.fileName.includes('node_modules'),
+    );
+  }
+
+  return true;
+};
