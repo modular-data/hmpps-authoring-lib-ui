c7bb1ac944798d87ff9662aac4d9e1d04a000637
Oleksandr Kolesnikov <kolesnikovo@mydigicode.com>
2026-03-12 00:31:53 +0200

    MOJ-498 | Prevent GDS legacy SASS deprecation warnings in app and Storybook
    * MOJ-498 | GDS legacy SCSS warnings fixed
    
    * MOJ-498 | App SCSS configuration to prevent legacy SCSS warnings from deps
    
    * MOJ-498 | Redundant code removed

--- apps/hmpps-authoring-lib-ui/next.config.js
+++ apps/hmpps-authoring-lib-ui/next.config.js
@@ -1,6 +1,5 @@
 //@ts-check
 
- 
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -10,6 +9,10 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  sassOptions: {
+    // TODO: Remove once govuk-frontend stops emitting Sass deprecation warnings from dependency imports.
+    quietDeps: true,
+  },
 };
 
 const plugins = [
--- libs/gds-components/.storybook/main.ts
+++ libs/gds-components/.storybook/main.ts
@@ -1,14 +1,18 @@
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 import { type StorybookConfig } from '@storybook/nextjs';
-import { reactDocgenPropFilter } from './config/react-docgen-prop-filter';
+import { reactDocgenPropFilter } from './config/react-docgen-prop-filter.ts';
 
 const config: StorybookConfig = {
   stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: ['@storybook/addon-docs'],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
-    options: {},
+    options: {
+      nextConfigPath: fileURLToPath(
+        new URL('./next.config.js', import.meta.url),
+      ),
+    },
   },
   typescript: {
     reactDocgen: 'react-docgen-typescript',
