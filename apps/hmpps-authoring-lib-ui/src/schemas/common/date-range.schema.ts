1bd28ceac02e53522e5de77bf8404d08883c3b49
Oleksandr Kolesnikov <kolesnikovo@mydigicode.com>
2026-03-11 09:10:07 +0200

    MOJ-422 | Dead-code removed

--- apps/hmpps-authoring-lib-ui/src/schemas/common/date-range.schema.ts
+++ apps/hmpps-authoring-lib-ui/src/schemas/common/date-range.schema.ts
@@ -1,5 +1,7 @@
 import z from 'zod';
 
+// TODO: Double-check usage of this schema. If not used, remove it.
+
 export const dateRangeSchema = z.object({
   from: z.string().trim().optional(),
   to: z.string().trim().optional(),
