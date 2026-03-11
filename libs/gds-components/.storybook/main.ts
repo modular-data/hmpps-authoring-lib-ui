2321eea18bbdb9d2de2ec8ec3313fc3da6a74c0b
Oleksandr Kolesnikov <kolesnikovo@mydigicode.com>
2026-03-12 00:17:11 +0200

    MOJ-498 | GDS legacy SCSS warnings fixed

--- libs/gds-components/.storybook/main.ts
+++ libs/gds-components/.storybook/main.ts
@@ -1,14 +1,24 @@
 import { fileURLToPath } from 'node:url';
-import { dirname } from 'node:path';
+import { dirname, join } from 'node:path';
 import { type StorybookConfig } from '@storybook/nextjs';
-import { reactDocgenPropFilter } from './config/react-docgen-prop-filter';
+import { reactDocgenPropFilter } from './config/react-docgen-prop-filter.ts';
 
 const config: StorybookConfig = {
   stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: ['@storybook/addon-docs'],
+  staticDirs: [
+    {
+      from: join(getAbsolutePath('govuk-frontend'), 'dist/govuk/assets'),
+      to: '/assets',
+    },
+  ],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
-    options: {},
+    options: {
+      nextConfigPath: fileURLToPath(
+        new URL('./next.config.js', import.meta.url),
+      ),
+    },
   },
   typescript: {
     reactDocgen: 'react-docgen-typescript',
