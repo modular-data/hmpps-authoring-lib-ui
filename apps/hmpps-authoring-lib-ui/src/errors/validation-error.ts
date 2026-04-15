7f3e90a2c7c2f9ec293e577926cdb7f8275f9aaa
Oleksandr Kolesnikov <kolesnikovo@mydigicode.com>
2026-04-15 08:45:46 +0300

    MOJ-455 | ValidationError modified by env cross-boundary detection

--- apps/hmpps-authoring-lib-ui/src/errors/validation-error.ts
+++ apps/hmpps-authoring-lib-ui/src/errors/validation-error.ts
@@ -1,13 +1,19 @@
+import { type ZodError } from 'zod';
+
 export type ValidationErrorPath = (string | number)[];
 
-interface ValidationErrorItem {
+export interface ValidationErrorItem {
   path: ValidationErrorPath;
   message: string;
 }
 
-type ValidationErrorItems = ValidationErrorItem[];
+export type ValidationErrorItems = ValidationErrorItem[];
+
+const VALIDATION_ERROR_TOKEN = Symbol.for('errors/ValidationError');
 
 export class ValidationError extends Error {
+  readonly [VALIDATION_ERROR_TOKEN] = true;
+
   private constructor(readonly errors: ValidationErrorItems) {
     super('Validation error happened!');
 
@@ -16,5 +22,26 @@ export class ValidationError extends Error {
 
   static fromApi(apiErrors: ValidationErrorItems): ValidationError {
     return new ValidationError(apiErrors);
+  }
+
+  static fromZod(zodError: ZodError): ValidationError {
+    return new ValidationError(
+      zodError.issues.map(({ path, message }) => ({
+        path: path.map((segment) => {
+          return typeof segment === 'number' ? segment : String(segment);
+        }),
+        message,
+      })),
+    );
+  }
+
+  static isValidationError(error: unknown): error is ValidationError {
+    if (typeof error !== 'object' || error === null) {
+      return false;
+    }
+
+    const candidate = error as Record<PropertyKey, unknown>;
+
+    return candidate[VALIDATION_ERROR_TOKEN] === true;
   }
 }
